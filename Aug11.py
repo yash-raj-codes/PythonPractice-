dbb2c4e139b93de62eb5b66cb3f0b571f1ec8721
Yash Raj <yashrajdelhi18@gmail.com>
2026-08-12 06:45:13 +0530

    CodewithHarry video 1-6 practice

---
+++ Aug11.py
@@ -0,0 +1,24 @@
+print("Hello world")
+print(5)
+print("Bye") 
+print(10+11.01)        # Why its output is 21.009999999999998
+print(11.011-2.0222)   
+print(11*12.022)
+print(17.055/13)
+
+# Hey this is used for comment
+print("Hey I am a \"good boy\"\nand this viewer is also a good boy")   # This is for new line.  also known as escape sequence characters.  to use these characters \" and \" like these things. or use single quates 
+
+print("Hey", 6, 7, sep = "~" , end ="009\n") #If not typing these genral sep =" " and end = "\n"
+print("This is Yash")
+
+a = 1
+print(a)
+
+print(type(a))
+b = "Yash"
+print(type(b))
+c = True
+print(type(c))
+d = None 
+print(d,type(d))
