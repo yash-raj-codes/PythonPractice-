1ccfb40176fc45be04334b43bd70994550393e6e
Yash Raj <yashrajdelhi18@gmail.com>
2026-08-18 10:43:54 +0530

    Todays lecture and yesterdays updating

--- Aug17.py
+++ Aug17.py
@@ -1,7 +1,22 @@
-num = -5
+num = -10
 
 if num >= 0:
     print("Positive")
 else:
     print("Negetive")
-    
+
+
+year = int(input())
+if(year&4==0):
+    print("Leap year")
+else:
+    print("Not a leap year")
+
+num = int(input())
+if(num%10==0):
+    print("Divisible by both 5 and 10")
+elif (num%5==0):
+    print("Divisible by 5 only")
+else:
+    print("Divisible by neither 5 nor 10")
+
---
+++ Aug18.py
@@ -0,0 +1,49 @@
+# range(start, stop,step)
+for i in range(5):
+    print(i)
+
+# range(5) generates numbers from 0 to 4, not 0 to 5
+
+numbers = list(range(1,6))
+print(numbers)
+
+print(list(range(5)))  # 0,1,2,3,4
+print(list(range(2,8))) # 0,1,2,3,4
+print(list(range(1,10,3)))  # 1,4,7
+print(list(range(10,0,-2))) # 10, 8, 6, 4, 2
+
+for i in range(1,6):
+    print(i)
+
+for ch in "python":
+    print(ch)
+
+for i in range(1,4):
+    for j in range(1,4):
+        print(i,j)
+
+print("")
+print("")
+print("")
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+print("")
+print("")
+
+for i in range(1,11):
+    if i == 5:
+        break
+    print(i)
+
+print("")
+print("")
+print("")
+
+for i in range(1,11):
+    if i == 5:
+        continue
+    print(i)
