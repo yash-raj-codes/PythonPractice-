035ab792370801f01acb1228c3d1d609b8fe3f1d
Yash Raj <yashrajdelhi18@gmail.com>
2026-08-23 12:48:25 +0530

    adding new Practice1.py file

--- Aug20.py
+++ Aug20.py
@@ -1,6 +1,21 @@
-num = int(input("Input a number(-1 to stop):"))
+# num = int(input("Input a number(-1 to stop):"))
 
-while num != -1:
-    print("You entered:", num)
-    num = int(input("Enter a number(-1 to stop):"))
+# while num != -1:
+#     print("You entered:", num)
+#     num = int(input("Enter a number(-1 to stop):"))
 
+
+# Practice 1
+marks = 0
+num = 0
+turns = 0
+while (marks>=0):
+    
+    num = int(input("Marks in the subjects(-1 if completed)"))
+    if (num == -1):
+        break
+    elif(num != -1):
+        marks += num
+        turns += 1
+
+print(marks/(turns))
