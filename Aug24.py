f99b8fa6242142872851c2a8329a7c0232ca92b1
Yash Raj <yashrajdelhi18@gmail.com>
2026-08-29 06:45:49 +0530

    Updating code

---
+++ Aug24.py
@@ -0,0 +1,2 @@
+a = "abaabbaabbaabbaabbaabb"
+print(a.replace("aabbaa","x"))
---
+++ Practice1.py
@@ -0,0 +1,48 @@
+print("Hello Yah This is 1st practice of 23 August 2026")
+r = 10
+area = 3.14*r*r
+print(area)
+
+rad = float(input("The radius of the circle is: "))
+# global manhunt
+area = 3.14*rad*rad
+print(area)
+str1 = "Yash Raj"
+str2 = "He is"
+print(str1 +" "+ str2)
+
+# import random
+# r = random.randint(1,20)
+
+# while(True):
+#     inp = int(input())
+#     if(inp<r):
+#         print("Wrong guess, try a greater number")
+#     elif(inp>r):
+#         print("Wrong guess, try a smaller number")
+#     else:
+#         print("Congrats, you've guessed the number")
+#         break;
+
+yash = 12
+
+# for x in range(6.1):
+#     print(x)
+
+def myfunc(a,b):
+    print(a+b)
+
+raj = 3.54
+
+myfunc(yash,raj)
+
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+print(a + b)
+
+fruit = "Mango"
+len1 = len(fruit)
+print(len1)
+
+
+print(fruit[-1:len(fruit)-3])
