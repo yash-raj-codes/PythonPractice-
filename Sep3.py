611518b08f8b88cb01bf59b0f1b0205d2e6b0c49
Yash Raj <yashrajdelhi18@gmail.com>
2026-09-04 11:18:19 +0530

    Adding 3Sep.py file this time

---
+++ Sep3.py
@@ -0,0 +1,13 @@
+data = {10, "Python", 3.14, True}
+marks = [78, 85, 92, 88]
+print("Number of students:", len(marks))
+print("Higest marks:", max(marks))
+print("Lowest marks:", min(marks))
+print("Total marks:", sum(marks))
+print("Sorted marks:", sorted(marks))
+
+student = {
+    "name" : "Rahul",
+    "age" : 20,
+    "course" : "Python"
+}
