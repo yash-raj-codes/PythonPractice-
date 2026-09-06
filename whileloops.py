f28cd0fbd96f976fb408897b54651c3bd0ccab27
Yash Raj <yashrajdelhi18@gmail.com>
2026-09-06 13:26:46 +0530

    adding while loops practicing

---
+++ whileloops.py
@@ -0,0 +1,23 @@
+for i in range(3):
+    print(i)
+
+print()
+
+i = 0
+while(i<=3):
+    print(i)
+    i = i+1
+
+print("")
+
+print("Done with the loop")
+
+print()
+
+i = int(input("Enter hte number: "))
+while(i<=38):
+    i = int(input("Enter hte number: "))
+    print(i)
+
+print("")
+
